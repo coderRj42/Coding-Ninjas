e98b770471b6f1ac83806736a39ad855a7ed7851
Jai Kumar Dadhich <84243610+coderRj42@users.noreply.github.com>
2021-09-11 00:10:32 +0530

    Create boolean varibke.java

---
+++ Introduction to Java/Getting Started with Java/boolean varibke.java
@@ -0,0 +1,10 @@
+ Boolean Variable
+Send Feedback
+Which of these values can be assigned to a boolean variable in Java?
+
+
+Options
+Correct Answer
+Solution Description
+
+Boolean variable in java can only hold either true or false value.
