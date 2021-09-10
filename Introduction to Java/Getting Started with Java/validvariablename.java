80e0296f3981249c6650eaa8890f3cfb73c2b899
Jai Kumar Dadhich <84243610+coderRj42@users.noreply.github.com>
2021-09-11 00:11:49 +0530

    Create validvariablename.java

---
+++ Introduction to Java/Getting Started with Java/validvariablename.java
@@ -0,0 +1,8 @@
+ Valid Variable Name
+Send Feedback
+Which of these is a valid variable name ?
+   
+    Correct Answer
+Solution Description
+
+In java, you cannot start your variable name with numbers and also they cannot contain any other special character except underscore (_) and dollar ($).
