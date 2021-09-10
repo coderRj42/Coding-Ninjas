03082aa7a15aa7f310d65dba6f93f4f89559a70d
Jai Kumar Dadhich <84243610+coderRj42@users.noreply.github.com>
2021-09-11 00:09:18 +0530

    Create datatype.java
     Datatype
    Send Feedback
    Which of the following data type stores longest decimal number ?

---
+++ Introduction to Java/Getting Started with Java/datatype.java
@@ -0,0 +1,4 @@
+ Correct Answer Double
+Solution Description
+
+Out of all given options, only float and double can hold decimal numbers. Size of the float is 4 bytes and double is 8 bytes (in most of the compilers, as the size of data types is compiler specific). So double can store bigger decimal numbers.
