927bcf895b7e7462c9c6e96bcaa381e331c4fccf
Jai Kumar Dadhich <84243610+coderRj42@users.noreply.github.com>
2021-09-23 21:58:40 +0530

    Create Alpha Pattern.java
    Print the following pattern for the given N number of rows.
    Pattern for N = 3
     A
     BB
     CCC
    Input format :
    Integer N (Total no. of rows)
    Output format :
    Pattern in N lines
    Constraints
    0 <= N <= 26
    Sample Input 1:
    7
    Sample Output 1:
    A
    BB
    CCC
    DDDD
    EEEEE
    FFFFFF
    GGGGGGG
    Sample Input 2:
    6
    Sample Output 2:
    A
    BB
    CCC
    DDDD
    EEEEE
    FFFFFF

---
+++ Introduction to Java/Patterns - 1/Alpha Pattern.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Solution {
+public static void main(String[] args) {
+Scanner s = new Scanner(System.in);
+int n = s.nextInt();
+int currRow = 1;
+while(currRow <= n) {
+int currCol = 1;
+char ch = (char)('A' + currRow - 1);
+while(currCol <= currRow) {
+System.out.print(ch);
+currCol += 1;
+}
+System.out.println();
+currRow += 1;
+}
+}
+}
